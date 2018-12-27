82619ffb08a31f193c61fbd74028a7e8f1193444
lponisio <lponisio@gmail.com>
2018-12-27 11:49:29 -0800

    setup for perry debugging

--- analysis/nmixture/run.R
+++ analysis/nmixture/run.R
@@ -3,36 +3,37 @@ rm(list=ls())
 setwd("analysis/nmixture")
 source('src/initialize.R')
 
+## all sampler options
 ## cust.MCMCs <- c('nimble', 'jags', 'block_RW', 'jags_like_nimble','block_AFSS')
 ## MCMC.defs <- c('nimble', 'jags', MCMCdefs.RW.block, MCMCdefs.slice,
 ##                MCMCdefs.AFSS.block)
 
+## perry, perhaps try first wiht just nimble
+cust.MCMCs <- c('nimble')
+MCMC.defs <- c('nimble')
 
-## cust.MCMCs <- c('nimble', 'jags')
-## MCMC.defs <- c('nimble', 'jags')
+names(MCMC.defs) <- cust.MCMCs
 
-## names(MCMC.defs) <- cust.MCMCs
+## FALSE for model integrating over latent states
+latent.opts <- c(FALSE)
+## true for model including random effects
+hyper.param.opts <- c(TRUE, FALSE)
 
-## ## FALSE for model integrating over latent states
-## latent.opts <- c(FALSE)
-## ## true for model including random effects
-## hyper.param.opts <- c(TRUE, FALSE)
-
-## for(h in hyper.param.opts){
-##     for(ff in latent.opts) {
-##         if(ff){ ## latent states and hyper param
-##             these.MCMCs <- cust.MCMCs
-##         }else{
-##             these.MCMCs <- cust.MCMCs[-2]
-##         }
-##         runAllModels(latent=ff,
-##                      hyper.param=h,
-##                      niter=niter,
-##                      burnin=burnin,
-##                      MCMCs=these.MCMCs,
-##                      MCMCdefs=MCMC.defs)
-##     }
-## }
+for(h in hyper.param.opts){
+    for(ff in latent.opts) {
+        if(ff){ ## latent states and hyper param
+            these.MCMCs <- cust.MCMCs
+        }else{
+            these.MCMCs <- cust.MCMCs
+        }
+        runAllModels(latent=ff,
+                     hyper.param=h,
+                     niter=niter,
+                     burnin=burnin,
+                     MCMCs=these.MCMCs,
+                     MCMCdefs=MCMC.defs)
+    }
+}
 
 
 effsHP <- getEffFUN("hyperparamTRUE", save.dir,  summary="efficiency")
