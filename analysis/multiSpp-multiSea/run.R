9bbd115b890b6ec1c2320dac6e0ad027bf24906b
Lauren <lauren@Jeffreys-iMac.local>
2018-12-26 17:40:43 -0800

    plotting and naming perfected

--- analysis/multiSpp-multiSea/run.R
+++ analysis/multiSpp-multiSea/run.R
@@ -12,27 +12,27 @@ names(MCMC.defs) <- cust.MCMCs
 
 
 
-## ## FALSE for model integrating over latent states
-## latent.opts <- c(TRUE, FALSE)
-## ## true for model including hyper paramters for year effects on phi,
-## ## gamma and p
-## hyper.param.opts <- c(FALSE)
-## for(h in hyper.param.opts){
-##     for(l in latent.opts) {
-##         if(l){ ## latent states
-##             these.MCMCs <- cust.MCMCs
-##         } else{ ## no latent states, but yes hyper param, cannot use JAGS
-##             these.MCMCs <- cust.MCMCs[-2]
-##         }
+## FALSE for model integrating over latent states
+latent.opts <- c(TRUE, FALSE)
+## true for model including hyper parameters for year effects on phi,
+## gamma and p
+hyper.param.opts <- c(FALSE)
+for(h in hyper.param.opts){
+    for(l in latent.opts) {
+        if(l){ ## latent states
+            these.MCMCs <- cust.MCMCs
+        } else{ ## no latent states, but yes hyper param, cannot use JAGS
+            these.MCMCs <- cust.MCMCs[-2]
+        }
 
-##         runAllModels(latent=l,
-##                      hyper.param=h,
-##                      niter=niter,
-##                      burnin=burnin,
-##                      MCMCs=these.MCMCs,
-##                      MCMCdefs=MCMC.defs)
-##     }
-## }
+        runAllModels(latent=l,
+                     hyper.param=h,
+                     niter=niter,
+                     burnin=burnin,
+                     MCMCs=these.MCMCs,
+                     MCMCdefs=MCMC.defs)
+    }
+}
 
 effsHP <- getEffFUN("hyperparamTRUE", save.dir,  summary="efficiency",
                     make.plot=FALSE)
